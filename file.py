271110ed187d7057fbe6a395dd6d825508ecef29
Moreira <im.senne2017@gmail.com>
2020-11-19 00:57:10 +0000

    Updates

--- file.py
+++ file.py
@@ -7,23 +7,34 @@ urlLivros='https://www.bibliaonline.com.br/acf/sl/113'
 reqLivros = requests.get(urlLivros)
 soupLivros = BeautifulSoup(reqLivros.content,'html.parser')
 linkLivros = soupLivros.find('article').find_all('p')
+#-----------Apenas teste ... Pegar do array pronto-------------
+urlTitulo='https://www.bibliaonline.com.br/acf/lc'
+reqTitulo= requests.get(urlTitulo)
+soupTitulo = BeautifulSoup(reqTitulo.content,'html.parser')
+#-------------------------------------------------------------------
+titulo = soupTitulo.find('h1',{ "class":"MuiTypography-root MuiTypography-h1"})
+
 sl = open('salmos.json','a+',encoding='utf8')
-sl.write('{ \n "c1": { \n')
+sl.write('{ \n "'+ titulo.get_text() +'": [')
 numVer=1
-while (numVer < len(soupLivros.find_all('sup'))):
-     for s in soupLivros.find_all('sup'):
-          s.clear()
-     for versiculo in linkLivros:
-          completo = '"' + str(numVer) + '" : "' + versiculo.get_text() + '",\n'
-          sl.write(completo)
-          numVer += 1
-sl.write('} \n }')     
-               
-
-     #versiculo_json = json.dumps(versiculo.get_text(),ensure_ascii=False).encode('utf-8')
-     #completo = '{ '+ '"'+str(1)+'" : "' + versiculo.get_text() + '"},\n'
-     #sl.write(completo)
-
+numCap = 50
+numTeste =1
+while numTeste < numCap:
+     sl.write('{ \n        "v":[ \n')
+     while (numVer < len(soupLivros.find_all('sup'))):
+          for s in soupLivros.find_all('sup'):
+               s.clear()
+          for versiculo in linkLivros:
+               if (numVer == len(soupLivros.find_all('sup'))):
+                    completo = '           { "num": ' + str(numVer) + ', "t": "' + versiculo.get_text() + '" }\n'
+                    sl.write(completo)
+               else:
+                    completo = '           { "num": ' + str(numVer) + ', "t": "' + versiculo.get_text() + '" },\n'
+                    sl.write(completo)
+                    numVer += 1
+     numTeste += 1
+     sl.write(']}, \n ')
+sl.write('\n ]}')
 sl.close()
     
 
